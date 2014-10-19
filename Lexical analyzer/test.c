f40cef9b60af1ff583bbfac6e8efb248d4f799ce
thinknight <jeeyeyeah@gmail.com>
2014-10-19 19:14:09 +0800

    first commit

---
+++ Lexical analyzer/test.c
@@ -0,0 +1,12 @@
+#include <stdio.h> //JIYE is a foolish
+int main()/asdas
+{
+    printf("Hello World!");
+    int a = 0;
+    a++;
+    /*
+    chen sen da sha bi
+    */
+    a <= b;
+    return 0;//hehe
+}
